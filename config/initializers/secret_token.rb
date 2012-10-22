d2fd49193b00edfc88ffc5e54c79e2d3887b8229
petrero <petrero@gmail.com>
2012-10-22 17:07:21 +0300

    application for deploying

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogProduction::Application.config.secret_token = '14d1ff19f9d9e8c4db3d2f0779737c01a78ca5f8b0284f8aeaa6f3e403c75332866fc352c62c6bf6cb9522c5e588f100d69afa4ac1d39ff0c502be714e6feef7'
