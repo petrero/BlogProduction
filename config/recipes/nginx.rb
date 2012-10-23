7299aad6a3b885e661cefd94203ce2a2ae20d4ff
petrero <petrero@gmail.com>
2012-10-23 12:41:11 +0300

    install nginx through capistrano recipe

--- config/recipes/nginx.rb
+++ config/recipes/nginx.rb
@@ -1,4 +1,11 @@
 namespace :nginx do
+	task :install do
+		run "#{sudo} add-apt-repository ppa:nginx/stable"
+		run "#{sudo} apt-get -y update"
+		run "#{sudo} apt-get -y install nginx"
+	end
+	after "deploy:install", "nginx:install"
+
 	task :setup do
 		# put result, "etc/nginx/sites-enabled/#{application}" (require special privileges)
 		template "nginx_unicorn.erb", "/tmp/nginx.conf"
