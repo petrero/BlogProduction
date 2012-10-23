50ab55b45e0a5dee681bd7a61d90a7b9aeae2927
petrero <petrero@gmail.com>
2012-10-23 10:33:06 +0300

    running tasks on multiple servers

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,11 @@
-server "192.168.33.10", :web
+server "192.168.33.10", :web, :app, :db, primary: true #this is the one that it's going to run migrations on
 set :user, "deployer"
 
 set :recipient, "Ruby"
 
 default_run_options[:pty] = true #if it's requested a password on the server will be passed to your shell
 
-task :hello do
+task :hello, roles: :db do
 	puts "Hello #{fetch(:recipient, "World!")}"
 	run "#{sudo} cp ~/hello /hello"
 end
